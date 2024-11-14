e9f69f19a53ed2b50163ac53e594e5d5acb70dfe
Alena Korma <alena.gorova93@gmail.com>
2024-11-14 14:21:22 +0200

    commit message

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const totalElementArray = firstArray.concat(secondArray);
+
+  if (totalElementArray.length > maxLength) {
+    const copyTotatArray = totalElementArray.slice(0, maxLength);
+    return copyTotatArray;
+  } else {
+    return totalElementArray;
+  }
+}
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
